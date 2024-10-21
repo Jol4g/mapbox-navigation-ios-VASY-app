4af53e1e74e33a5f8f831bd24c662a9e4b3f3be2
fedisarray <fedisarray@gmail.com>
2024-10-21 13:46:39 +0100

    patch files with VASY theme

--- Sources/MapboxNavigation/NavigationView.swift
+++ Sources/MapboxNavigation/NavigationView.swift
@@ -1,12 +1,12 @@
-InstructionsBannerView.swiftimport UIKit
+import UIKit
 import MapboxDirections
 import MapboxCoreNavigation
 
 /**
  A view that represents the root view of the MapboxNavigation drop-in UI.
-
+ 
  ## Components
-
+ 
  1. InstructionsBannerView
  2. InformationStackView
  3. BottomBannerView
@@ -15,7 +15,7 @@ import MapboxCoreNavigation
  6. FloatingStackView
  7. NavigationMapView
  8. SpeedLimitView
-
+ 
  ```
  +--------------------+
  |         1          |
@@ -38,27 +38,27 @@ import MapboxCoreNavigation
  */
 @IBDesignable
 open class NavigationView: UIView {
-
+    
     enum Constants {
         static let endOfRouteHeight: CGFloat = 260.0
         static let buttonSpacing: CGFloat = 8.0
     }
-
+    
     var compactConstraints = [NSLayoutConstraint]()
     var regularConstraints = [NSLayoutConstraint]()
-
+    
     // MARK: Data Configuration
-
+    
     weak var delegate: NavigationViewDelegate? {
         didSet {
             updateDelegates()
         }
     }
-
+    
     private func updateDelegates() {
         navigationMapView.delegate = delegate
     }
-
+    
     /**
      `NavigationMapView` that is displayed inside the `NavigationView`.
      */
@@ -66,12 +66,12 @@ open class NavigationView: UIView {
         didSet {
             oldValue.removeFromSuperview()
             insertSubview(navigationMapView, at: 0)
-
+            
             navigationMapView.isHidden = false
             navigationMapView.translatesAutoresizingMaskIntoConstraints = false
             navigationMapView.delegate = delegate
             navigationMapView.pinTo(parentView: self)
-
+            
             // FIXME: Provide a reliable way of notifying dependants (e.g. `CameraController`,
             // `ArrivalController` might need to re-subscribe to notifications that are sent from
             // injected `NavigationMapView` instance).
@@ -80,38 +80,38 @@ open class NavigationView: UIView {
             }
         }
     }
-
+    
     // MARK: End of Route UI
-
+    
     lazy var endOfRouteShowConstraint: NSLayoutConstraint? = endOfRouteView?.bottomAnchor.constraint(equalTo: bottomAnchor)
-
+    
     lazy var endOfRouteHeightConstraint: NSLayoutConstraint? = endOfRouteView?.heightAnchor.constraint(equalToConstant: Constants.endOfRouteHeight)
-
+    
     var topBannerContainerViewLayoutConstraints: [NSLayoutConstraint] = []
-
+    
     var bottomBannerContainerViewLayoutConstraints: [NSLayoutConstraint] = []
-
+    
     var endOfRouteViewLayoutConstraints: [NSLayoutConstraint] = []
-
+    
     var endOfRouteView: UIView? {
         didSet {
             if let active: [NSLayoutConstraint] = constraints(affecting: oldValue) {
                 NSLayoutConstraint.deactivate(active)
             }
-
+            
             oldValue?.removeFromSuperview()
             if let eor = endOfRouteView { addSubview(eor) }
             endOfRouteView?.translatesAutoresizingMaskIntoConstraints = false
         }
     }
-
+    
     func showEndOfRoute() {
         endOfRouteView?.isHidden = false
         setupEndOfRouteConstraints()
     }
-
+    
     // MARK: Overlay Views
-
+    
     /**
      Stack view that contains floating buttons.
      */
@@ -121,19 +121,19 @@ open class NavigationView: UIView {
         stackView.spacing = Constants.buttonSpacing
         return stackView
     }()
-
+    
     var floatingStackViewLayoutGuide: UILayoutGuide? {
         didSet {
             setupConstraints()
         }
     }
-
+    
     var floatingButtonsPosition: MapOrnamentPosition = .topTrailing {
         didSet {
             setupConstraints()
         }
     }
-
+    
     /**
      The buttons to show floating on the map inside `floatingStackView`.
      */
@@ -143,15 +143,15 @@ open class NavigationView: UIView {
             setupStackViews()
         }
     }
-
+    
     lazy var resumeButton: ResumeButton = .forAutoLayout(hidden: true)
-
+    
     var wayNameViewLayoutGuide: UILayoutGuide? {
         didSet {
             setupConstraints()
         }
     }
-
+    
     /**
      A host view for `WayNameLabel` that shows a road name and a shield icon.
      */
@@ -161,18 +161,18 @@ open class NavigationView: UIView {
         wayNameView.containerView.clipsToBounds = true
         return wayNameView
     }()
-
+    
     var speedLimitViewLayoutGuide: UILayoutGuide? {
         didSet {
             setupConstraints()
         }
     }
-
+    
     /**
      A view that displays a speed limit.
      */
     public lazy var speedLimitView: SpeedLimitView = .forAutoLayout(hidden: true)
-
+    
     /**
      View that is used as a container for top banners. By default, for turn-by-turn navigation
      `NavigationViewController` presents `TopBannerViewController` in this banner
@@ -184,7 +184,7 @@ open class NavigationView: UIView {
         topBannerContainerView.translatesAutoresizingMaskIntoConstraints = false
         return topBannerContainerView
     }()
-
+    
     /**
      View that is used as a container for bottom banners. By default, for turn-by-turn navigation
      `NavigationViewController` presents `BottomBannerViewController` in this banner
@@ -196,7 +196,7 @@ open class NavigationView: UIView {
         bottomBannerContainerView.translatesAutoresizingMaskIntoConstraints = false
         return bottomBannerContainerView
     }()
-
+    
     func clearStackViews() {
         let oldFloatingButtons: [UIView] = floatingStackView.subviews
         for floatingButton in oldFloatingButtons {
@@ -204,18 +204,18 @@ open class NavigationView: UIView {
             floatingButton.removeFromSuperview()
         }
     }
-
+    
     func setupStackViews() {
         if let buttons = floatingButtons {
          // Editied :   floatingStackView.addArrangedSubviews(buttons)
         }
     }
-
+    
     // MARK: Initialization methods
-
+    
     /**
      Initializes a `NavigationView` instance with the specified parameters.
-
+     
      - parameter frame: The frame rectangle for the `NavigationView`.
      - parameter tileStoreLocation: Configuration of the `TileStore` location, where map tiles are stored.
      Value is ignored if custom `NavigationMapView` instance was provided. Use `nil` to disable onboard tile storage.
@@ -227,17 +227,17 @@ open class NavigationView: UIView {
                 navigationMapView: NavigationMapView? = nil) {
         navigationMapView?.translatesAutoresizingMaskIntoConstraints = false
         self.navigationMapView = navigationMapView ?? NavigationMapView(frame: frame, tileStoreLocation: tileStoreLocation)
-
+        
         super.init(frame: frame)
         commonInit()
     }
-
+    
     public required init?(coder decoder: NSCoder) {
         navigationMapView = NavigationMapView(frame: .zero)
         super.init(coder: decoder)
         commonInit()
     }
-
+    
     convenience init(delegate: NavigationViewDelegate,
                      frame: CGRect = .zero,
                      tileStoreLocation: TileStoreConfiguration.Location? = .default,
@@ -246,19 +246,19 @@ open class NavigationView: UIView {
         self.delegate = delegate
         updateDelegates() // this needs to be called because didSet's do not fire in init contexts.
     }
-
+    
     convenience init(delegate: NavigationViewDelegate) {
         self.init(frame: .zero)
         self.delegate = delegate
         updateDelegates() // this needs to be called because didSet's do not fire in init contexts.
     }
-
+    
     func commonInit() {
         DayStyle().apply()
         setupViews()
         setupConstraints()
     }
-
+    
     func setupViews() {
         let children: [UIView] = [
             navigationMapView,
@@ -269,14 +269,14 @@ open class NavigationView: UIView {
             topBannerContainerView,
             bottomBannerContainerView
         ]
-
+        
         addSubviews(children)
-
+        
         navigationMapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         navigationMapView.mapView.ornaments.options.compass.visibility = .hidden
         navigationMapView.pinTo(parentView: self)
     }
-
+    
     open override func prepareForInterfaceBuilder() {
         super.prepareForInterfaceBuilder()
         DayStyle().apply()
